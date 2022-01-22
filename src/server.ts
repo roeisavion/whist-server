95551936243e7fb6a4930c73dc0c5cd794fb12d8
roeisavion <68278395+roeisavion@users.noreply.github.com>
2022-01-22 20:16:45 +0200

    removing notes

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,11 @@ import _ from 'lodash';
 export const initServer = () => {
     const app : any = express()
     const httpServer = http.createServer(app);
-    // httpServer.listen(9090, () => console.log("Listening.. on 9090"))
     const websocketServer = ws.server ;
     const wsServer = new websocketServer({
         httpServer
     })
     const port =  process.env.PORT || 9091
-    // (process.env.PORT==null || process.env.PORT =="") ? port = 9091 : port = process.env.PORT ;
     httpServer.listen(port, () => console.log(`Listening on http port ${port}`))
     
     return wsServer;
