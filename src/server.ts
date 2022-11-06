79e518780d10f877b0be535473dd7d0724563e90
roeisavion <68278395+roeisavion@users.noreply.github.com>
2022-11-06 12:37:43 +0200

    added server key

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
+import https from 'https';
 import http from 'http';
 import * as ws from 'websocket';
 import express from 'express';
+import fs from 'fs';
 import _ from 'lodash';
 
 export const initServer = () => {
     const app: any = express()
 
-    const httpServer = http.createServer(app);
+    const credentials  = {
+        key: fs.readFileSync('./whist-server-key.pem'),
+        // cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+      };
+
+    const httpServer = https.createServer(credentials,app);
+    // const httpServer = http.createServer(app);
     const websocketServer = ws.server;
     const wsServer = new websocketServer({
         httpServer
@@ -16,3 +24,4 @@ export const initServer = () => {
 
     return [wsServer ,app] ;
 }
+
