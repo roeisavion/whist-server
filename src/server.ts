88bf857a43469f2ca2a1ff1f1efeb53b5fdf75e5
roeisavion <68278395+roeisavion@users.noreply.github.com>
2022-11-14 12:09:33 +0200

    removing certificates

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
-import https from 'https';
 import http from 'http';
 import * as ws from 'websocket';
 import express from 'express';
-import fs from 'fs';
 import _ from 'lodash';
 
 export const initServer = () => {
     const app: any = express()
-
-    const credentials  = {
-        key: fs.readFileSync('./whist-server-key.pem'),
-        // cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
-      };
-
-    const httpServer = https.createServer(credentials,app);
-    // const httpServer = http.createServer(app);
+    const httpServer = http.createServer(app);
     const websocketServer = ws.server;
     const wsServer = new websocketServer({
         httpServer
